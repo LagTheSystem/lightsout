8dcf6afe55dd9f1cde38d5c5cc83c1f49a4b1c4b
herobrick360 <53086599+herobrick360@users.noreply.github.com>
2021-05-23 07:28:37 -0500

    Create ledon.py

---
+++ ledon.py
@@ -0,0 +1,3 @@
+os.system("sudo bash -c \"echo 1 > /sys/class/leds/led0/trigger\"")
+os.system("sudo bash -c \"echo 1 > /sys/class/leds/led0/brightness\"")
+os.system("sudo bash -c \"echo 1 > /sys/class/leds/led1/brightness\"")
